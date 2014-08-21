8f86f601fa4a0d0f1c75e0184b6a993ac02b4a27
Hugh Bisset <hwb23@uclive.ac.nz>
2014-08-21 16:14:42 +1200

    wus changed to make more directly relavent to project

--- include/wus.c
+++ include/wus.c
@@ -1,34 +1,29 @@
-/*
-    FreeRTOS V6.0.5 - Copyright (C) 2009 Real Time Engineers Ltd.
+/**
+ * \file wus.c
+ * \brief Active suspension control pulse train reader module.
+ * \author Hugh Bisset
+ * \version 1.0
+ * \date 2014-08-21
+ */
 
-    This file is part of the FreeRTOS distribution.
+/* Copyright (C)
+ * 2014 - Hugh Bisset
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
-    ***NOTE*** The exception to the GPL is included to allow you to distribute
-    a combined work that includes FreeRTOS without being obliged to provide the
-    source code for proprietary components outside of the FreeRTOS kernel.
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-    more details. You should have received a copy of the GNU General Public 
-    License and the FreeRTOS license exception along with FreeRTOS; if not it 
-    can be viewed here: http://www.freertos.org/a00114.html and also obtained 
-    by writing to Richard Barry, contact details for whom are available on the
-    FreeRTOS WEB site.
-
-    1 tab == 4 spaces!
-
-    http://www.FreeRTOS.org - Documentation, latest information, license and
-    contact details.
-
-    http://www.SafeRTOS.com - A version that is certified for use in safety
-    critical systems.
-
-    http://www.OpenRTOS.com - Commercial support, development, porting,
-    licensing and training services.
-*/
 
 /* FreeRTOS includes. */
 #include "include/FreeRTOS.h"
@@ -40,27 +35,14 @@
 //#include "inc\hw_sysctl.h"
 #include "driverlib/sysctl.h"
 
-/* Demo includes. */
-#include "demo_code\basic_io.h"
-
 
 /*Task Modules*/
 #include "simulate_task.h"
 #include "uart_task.h"
 #include "ui_task.h"
 
-/* Used as a loop counter to create a very crude delay. */
-#define mainDELAY_LOOP_COUNT		( 0xfffff )
-
-
 /* The task function. */
 void vTaskFunction( void *pvParameters );
-
-/* Define the strings that will be passed in as the task parameters.  These are
-defined const and off the stack to ensure they remain valid when the tasks are
-executing. */
-const char *pcTextForTask1 = "Task 1 is running\n";
-const char *pcTextForTask2 = "Task 2 is running\n";
 
 const char *placeholder = "test";
 
@@ -71,20 +53,6 @@ int main( void )
 	/* Set the clocking to run from the PLL at 50 MHz.  Assumes 8MHz XTAL,
 	whereas some older eval boards used 6MHz. */
 	SysCtlClockSet( SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ );
-
-	/* Create one of the two tasks. */
-	xTaskCreate(	vTaskFunction,			/* Pointer to the function that implements the task. */
-					"Task 1",				/* Text name for the task.  This is to facilitate debugging only. */
-					240,					/* Stack depth in words. */
-					(void*)pcTextForTask1,	/* Pass the text to be printed in as the task parameter. */
-					1,						/* This task will run at priority 1. */
-					NULL );					/* We are not using the task handle. */
-
-	/* Create the other task in exactly the same way.  Note this time that we
-	are creating the SAME task, but passing in a different parameter.  We are
-	creating two instances of a single task implementation. */
-	xTaskCreate( vTaskFunction, "Task 2", 240, (void*)pcTextForTask2, 1, NULL );
-
 
     /*Simulates the road height position, acceration of the unsprung and sprung spring and the coil extension*/
 	xTaskCreate( vSimulateTask, "Simulate task", 240,(void*) placeholder , 1, NULL );
@@ -102,43 +70,8 @@ int main( void )
 	heap available for the idle task to be created. */
 	for( ;; );
 }
-/*-----------------------------------------------------------*/
 
 
-/**
- * \Inits the user input task
- *
- * \param pvParameters
- */
-
-
-
-
-void vTaskFunction( void *pvParameters )
-{
-char *pcTaskName;
-volatile unsigned long ul;
-
-	/* The string to print out is passed in via the parameter.  Cast this to a
-	character pointer. */
-	pcTaskName = ( char * ) pvParameters;
-
-	/* As per most tasks, this task is implemented in an infinite loop. */
-	for( ;; )
-	{
-		/* Print out the name of this task. */
-		vPrintString( pcTaskName );
-
-		/* Delay for a period. */
-		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
-		{
-			/* This loop is just a very crude delay implementation.  There is
-			nothing to do in here.  Later exercises will replace this crude
-			loop with a proper delay/sleep function. */
-		}
-	}
-}
-/*-----------------------------------------------------------*/
 
 void vApplicationMallocFailedHook( void )
 {
