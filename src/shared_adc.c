782afda8c1c7c88fe32bfa307f2b7c4918f77f40
Tom Walsh <tom.n75@gmail.com>
2014-09-01 15:52:18 +1200

    Added shared_adc.c, includes basic function blocks.

---
+++ src/shared_adc.c
@@ -0,0 +1,36 @@
+/**
+ * \file shared_adc.c
+ * \brief Common module allowing configuration and reading of ADC input.
+ * \author Tom Walsh
+ * \version 1.0
+ * \date 2014-09-01
+ */
+
+/* Copyright (C)
+ * 2014 - Tom Walsh
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+void initAdcModule(char adcs)
+{
+
+}
+
+
+int getSmoothAdc(char adc)
+{
+
+}
