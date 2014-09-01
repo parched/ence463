314d577a903feda4fbbef59534195d4c9e6e3a47
Hugh Bisset <hwb23@uclive.ac.nz>
2014-09-01 23:30:20 +1200

    pushing into master shared_guilayout and shared_pwm

--- include/shared_pwm.h
+++ include/shared_pwm.h
@@ -1,3 +1,4 @@
+
 /**
  * \file shared_pwm.h
  * \brief Common module allowing configuation of and reading of PWM outputs.
@@ -6,39 +7,40 @@
  * \date 2014-08-20
  */
 
-/* Copyright (C) 
+/* Copyright (C)
  * 2014 - James Duley
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  */
 
 #ifndef SHARED_PWM_H
 #define SHARED_PWM_H
 
-#define ACC_SPRUNG_PWM 0x01      /**< The sprung acceleration PWM. */
-#define ACC_UNSPRUNG_PWM 0x02    /**< The unsprung acceleration PWM. */
-#define COIL_EXTENSION_PWM 0x04  /**< The coil extension PWM. */
-#define ACTUATOR_FORCE_PWM 0x01  /**< The actuator force PWM. */
-#define DAMPING_COEFF_PWM 0x02   /**< The damping coefficient PWM. */
+#define ACC_SPRUNG_PWM 1
+#define ACC_UNSPRUNG_PWM 2
+#define COIL_EXTENSION_PWM 4
+#define DAMPING_PWM 1
+#define ACTUACTOR_PWM 4
+
 
 /**
  * \brief Initailises the PWM module.
  *
  * \param pwmOuts The bitwise OR of the PWM outputs init.
  */
-void initPwmModule(char pwmOuts);
+void initPwmModule(int pwmORed);
 
 /**
  * \brief Sets the PWM duty.
@@ -46,6 +48,6 @@ void initPwmModule(char pwmOuts);
  * \param pwmOut The PWM output of which to set.
  * \param duty The PWM duty cycle times 1000.
  */
-void setDuty(char pwmOut, int duty);
+void setDuty(int pwmPin, int duty);
 
 #endif
