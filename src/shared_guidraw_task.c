c979d8fde4c54ecdaf9f2f749d1fb3b2b60d1c68
George Xian <gjx10@uclive.ac.nz>
2014-09-22 17:35:59 +1200

    Priority shifts for gui related tasks. Guidraw task sleep corrected

--- src/shared_guidraw_task.c
+++ src/shared_guidraw_task.c
@@ -36,7 +36,7 @@
 #include "shared_displayformat128x64.h"
 
 #define INPUTEVENT_QUEUE_SIZE 10
-#define GUITASK_SLEEP_MS 40
+#define GUI_TASK_RATE_HZ 25
 #define OLED_FREQ 1000000
 
 typedef enum {VERTDIR_UP, VERTDIR_DOWN} VertDir;
@@ -157,7 +157,7 @@ void vGuiRefreshTask(void *pvParameters)
 
 	// initialize FreeRTOS sleep parameters
 	portTickType xLastWakeTime;
-	const portTickType xFrequency = GUITASK_SLEEP_MS*portTICK_RATE_MS;
+	const portTickType xTimeIncrement = configTICK_RATE_HZ / GUI_TASK_RATE_HZ;
 	xLastWakeTime = xTaskGetTickCount();
 
 	// initialize screen
@@ -167,7 +167,7 @@ void vGuiRefreshTask(void *pvParameters)
 	for (;;)
 	{
 		// wait for next cycle
-		vTaskDelayUntil( &xLastWakeTime, xFrequency);
+		vTaskDelayUntil( &xLastWakeTime, xTimeIncrement);
 
 		// updates read only values
 		refreshReadonlyValues(unitActivity);
