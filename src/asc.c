063e58b6afb57ce36c490a6ccd7a917fd7845057
Tom Walsh <tom.n75@gmail.com>
2014-10-08 11:56:37 +1300

    Changed slashes to unix style "/"

--- src/asc.c
+++ src/asc.c
@@ -30,9 +30,9 @@
 #include "task.h"
 
 /* Stellaris library includes. */
-#include "inc\hw_types.h"
-#include "inc\hw_memmap.h"
-#include "driverlib\sysctl.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
 
 
 /*Task Modules*/
