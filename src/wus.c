eb48463ac4b79efa4fd2e09c476edaeccfbb994a
Hugh Bisset <hwb23@uclive.ac.nz>
2014-10-10 18:12:41 +1300

    removed comment

--- src/wus.c
+++ src/wus.c
@@ -110,7 +110,6 @@ int main(void)
 	xTaskCreate(vButtonPollingTask, "Button polling task", 240, (void*) placeholder, 2, NULL);
 
 	/* Start the scheduler so our tasks start executing. */
-	/*remove following comment*/
 	vTaskStartScheduler();
 	
 	/* If all is well we will never reach here as the scheduler will now be
