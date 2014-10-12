1b08453208710f3b9ed5971fe126c7195f2bea2a
Hugh Bisset <hwb23@uclive.ac.nz>
2014-10-12 17:00:51 +1300

    122-error-handling prototype not fully implemente

---
+++ include/shared_errors.h
@@ -0,0 +1,37 @@
+/**
+ * \file shared_errors.h
+ * \brief file that first runs in the wheel unit simulator
+ * \author Hugh Bisset
+ * \version 1.0
+ * \date 2014-10-12
+ */
+
+/* Copyright (C)
+ * 2014 - Hugh Bisset
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#ifndef SHARED_ERRORS_H_
+#define SHARED_ERRORS_H_
+
+int okStatus = 0;
+int powerFailure = 0;
+int sprungAccExceeded = 0;
+int unsprungAccExceeded = 0;
+int heightExceeded = 0;
+int speedExceeded = 0;
+
+#endif /* SHARED_ERRORS_H_ */
